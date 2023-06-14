a747f392e1ebf6b52d88f6b3169014de181c4764
akyrey <stein.dario@gmail.com>
2023-06-14 12:15:27 +0200

    chore: add example transloco.config.js file

---
+++ transloco.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  rootTranslationsPath: 'assets/i18n',
+  langs: ['el', 'en', 'it'],
+  keysManager: {},
+  scopePathMap: {
+    audit: 'src/app/modules/audit/i18n',
+    contacts: 'src/app/modules/contacts/i18n',
+    cookiePolicy: 'src/app/modules/cookie-policy/i18n',
+    cookiePolicyEn: 'src/app/modules/cookie-policy-en/i18n',
+    dashboard: 'src/app/modules/dashboard/i18n',
+    privacyPolicy: 'src/app/modules/privacy-policy/i18n',
+    privacyPolicyEn: 'src/app/modules/privacy-policy-en/i18n',
+    settings: 'src/app/modules/settings/i18n',
+    shareElement: 'src/app/modules/share-element/i18n',
+    signature: 'src/app/modules/signature/i18n',
+    termsAndConditions: 'src/app/modules/terms-and-conditions/i18n',
+  },
+};
